2c1c3a00f108f0ce5c058003203c96629c211c94
rikosalem01 <rikosalem01@gmail.com>
2023-05-17 22:13:12 +0700

    final1

--- src/component/Experience.jsx
+++ src/component/Experience.jsx
@@ -1,19 +1,11 @@
 import React from "react";
 
 import html from "../assets/html.png";
-
 import css from "../assets/css.png";
-
 import javascript from "../assets/javascript.png";
-
 import reactImage from "../assets/react.png";
-
 import nextjs from "../assets/nextjs.png";
-
-import graphql from "../assets/graphql.png";
-
 import github from "../assets/github.png";
-
 import tailwind from "../assets/tailwind.png";
 
 const Experience = () => {
@@ -51,17 +43,12 @@ const Experience = () => {
     {
       id: 6,
       src: nextjs,
-      title: "NextJs",
+      title: "Next JS",
       style: "shadow-white",
     },
+
     {
       id: 7,
-      src: graphql,
-      title: "GraphQl",
-      style: "shadow-pink-400",
-    },
-    {
-      id: 8,
       src: github,
       title: "GitHub",
       style: "shadow-gray-400",
@@ -69,17 +56,17 @@ const Experience = () => {
   ];
 
   return (
-    <div name="experience" className=" bg-gradient-to-b from-gray-800 to-black w-full h-screen">
-      <div className=" max-w-screen-lg mx-auto flex flex-col justify-center w-full text-white">
+    <div name="experience" className="bg-gradient-to-b from-gray-800 to-black w-full h-screen">
+      <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
         <div>
-          <p className=" text-xl font-bold border-b-4 bg-gray-500 p-2 inline">Experience</p>
-          <p className="py-6">These are the techlology i've worked with</p>
+          <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline">Experience</p>
+          <p className="py-6">These are the technologies I've worked with</p>
         </div>
 
-        <div className=" w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
+        <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
           {techs.map(({ id, src, title, style }) => (
             <div key={id} className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}>
-              <img src={src} alt="" className=" w-20 mx-auto" />
+              <img src={src} alt="" className="w-20 mx-auto" />
               <p className="mt-4">{title}</p>
             </div>
           ))}
--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -6,26 +6,28 @@ import { Link } from "react-scroll";
 const Home = () => {
   return (
     <div name="home" className=" h-screen w-full bg-gradient-to-b from-black via-black to-gray-800">
-      <div
-        className=" max-w-screen-lg mx-auto flex flex-col
-     items-center justify-center h-full px-4 md:flex-row"
-      >
+      <div className=" max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className=" flex flex-col justify-center h-full lg:w-1/2">
-          <h2 className=" text-4xl sm:text-7xl sm:mt-5 font-bold text-white">I'm a Full Stack Developer</h2>
+          <h2 className="text-4xl sm:text-7xl font-bold text-white">I'm a Full Stack Developer</h2>
           <p className=" text-gray-500 py-4 max-w-md">
             Saya sedang belajar Full Stack Developer di Harisenin.com untuk mengupgrade skill dan sekarang saya terbiasa menggunakan React, tailwind, NexJs, GraphQl dalam membangun sebuah aplikasi
           </p>
           <div>
-            <Link to="portfolio" smooth duration={500} className=" group text-white w-fit px-6 py-3 my-2 flex item-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer">
-              portofolio
-              <span className=" group-hover:rotate-90 duration-300">
+            <Link to="portfolio" smooth duration={500} className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer">
+              Portfolio
+              <span className="group-hover:rotate-90 duration-300">
                 <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
               </span>
             </Link>
           </div>
         </div>
-        <div className=" lg:w-1/2">
-          <img src={HeroImage} alt="riko salem" className=" rounded-2xl mx-auto max-w-full md:w-full" />
+        <div className=" ">
+          <img
+            src={HeroImage}
+            alt="riko salem"
+            className="rounded-2xl mx-auto w-2/3 ml: 
+          md:w-full "
+          />
         </div>
       </div>
     </div>
