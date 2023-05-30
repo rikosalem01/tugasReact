b35782e27e4103e90e7f10a00d7608a5bc3ac24c
rikosalem01 <rikosalem01@gmail.com>
2023-05-30 19:55:43 +0700

    experience update

--- src/component/Experience.jsx
+++ src/component/Experience.jsx
@@ -59,8 +59,8 @@ const Experience = () => {
     <div name="experience" className="bg-gradient-to-b from-gray-800 to-black w-full h-screen">
       <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
         <div>
-          <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline">Pengalaman</p>
-          <p className="py-6">Aplikasi yang biasa di gunakan</p>
+          <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline">Tect Stack</p>
+          <p className="py-6">These are the technologies I've worked with</p>
         </div>
 
         <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
